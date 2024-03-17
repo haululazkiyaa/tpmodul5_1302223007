a7cfcdea659e455832d4d93ad09882f83f4690e2
Muhammad Haulul Azkiyaa <haululazkiyaa@Muhammads-MacBook-Air.local>
2024-03-17 16:38:11 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223007/Program.cs
+++ tpmodul5_1302223007/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataGeneric<String> data = new DataGeneric<String>("1302223007");
+        data.PrintData();
+    }
+}
 
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah " + data);
+    }
+}
