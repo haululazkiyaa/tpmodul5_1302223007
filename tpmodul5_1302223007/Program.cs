dda47e65ae8376959ea4bffa70af5a90cda4c8e9
Muhammad Haulul Azkiyaa <haululazkiyaa@Muhammads-MacBook-Air.local>
2024-03-17 16:29:51 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_1302223007/Program.cs
+++ tpmodul5_1302223007/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<String>("Haulul");
+    }
+}
 
+class HaloGeneric
+{
+    public static void SapaUser<T>(T x)
+    {
+        Console.WriteLine("Halo user " + x);
+    }
+}
